58bae2acb5f39ea38bca083c7f6e5a8faed1a9b6
Shuang Shan <shuang_nju@163.com>
2020-09-10 11:38:08 +0100

    add: chap 4 cluster

--- CovMatrix.py
+++ CovMatrix.py
@@ -10,8 +10,7 @@
 
 import numpy as np,pandas as pd
 import matplotlib.pyplot as plt
-from tqdm.notebook import tqdm
-
+from tqdm import tqdm
 
 # ## The Marcenko-Pastur Theorem
 # 
@@ -67,20 +66,6 @@ def fitKDE(obs,bWidth=.25,kernel='gaussian',x=None):
     return pdf
 
 
-# In[4]:
-
-
-#--------------------------------------------------- 
-x=np.random.normal(size=(10000,1000)) 
-eVal0,eVec0=getPCA(np.corrcoef(x,rowvar=False)) # each column is a variable
-pdf0=mpPDF(1.,q=x.shape[0]/float(x.shape[1]),pts=1000) 
-pdf1=fitKDE(np.diag(eVal0),bWidth=.01) # empirical pdf
-ax = plt.figure().add_subplot(111)
-ax.plot(pdf0,label= 'Marcenko-Pastur')
-ax.plot(pdf1,linestyle = '--',label= 'Empirical:KDE')
-ax.set_xlabel(r'$\lambda$')
-ax.set_ylabel(r'prob[$\lambda$]')
-ax.legend()
 
 
 # 
@@ -102,15 +87,7 @@ def cov2corr(cov):
     corr=cov/np.outer(std,std) 
     corr[corr<-1],corr[corr>1]=-1,1 # numerical error 
     return corr
-#--------------------------------------------------- 
-alpha,nCols,nFact,q=.995,1000,100,10 
-cov=np.cov(np.random.normal(size=(nCols*q,nCols)),rowvar=False) 
-cov=alpha*cov+(1-alpha)*getRndCov(nCols,nFact) # noise+signal 
-corr0=cov2corr(cov)
-eVal0,eVec0=getPCA(corr0)
 
-
-# In[6]:
 
 
 #SNIPPET 2.4 FITTING THE MARCENKO–PASTUR PDF 
@@ -134,9 +111,19 @@ def findMaxEval(eVal,q,bWidth):
         var=1 
     eMax=var*(1+(1./q)**.5)**2
     return eMax,var
+
+# ax = plt.figure().add_subplot(111)
+# ax.plot(pdf0,label= 'Marcenko-Pastur')
+# ax.plot(pdf1,linestyle = '--',label= 'Empirical:KDE')
+# ax.set_xlabel(r'$\lambda$')
+# ax.set_ylabel(r'prob[$\lambda$]')
+# ax.legend()
+
+
+#---------------------------------------------------
+
 #--------------------------------------------------- 
-eMax0,var0=findMaxEval(np.diag(eVal0),q,bWidth=.01) 
-nFacts0=eVal0.shape[0]-np.diag(eVal0)[::-1].searchsorted(eMax0)
+
 
 # nFacts0 gives the number of the eigenvalue is assumed to be important (cutoff level lambda+ adjusted for the presence of nonrandom eigenvectors)
 
@@ -147,21 +134,21 @@ nFacts0=eVal0.shape[0]-np.diag(eVal0)[::-1].searchsorted(eMax0)
 #---------------------------------------------------
 # Fitting the Marcenko–Pastur PDF on a noisy covariance matrix.
 # estimate the sigma for Marcenko-Pastur dist
-bWidth=0.01
-out=minimize(lambda *x: errPDFs(*x),.5,args=(np.diag(eVal0),q,bWidth),bounds=((1E-5,1-1E-5),))
-if out['success']:
-    var=out['x'][0] 
-else:
-    var=1
-
-pdf0=mpPDF(var,q,pts=1000) # Marcenko-Pastur dist
-pdf1=fitKDE(np.diag(eVal0),bWidth=.01) # empirical pdf
-ax = plt.figure().add_subplot(111)
-ax.plot(pdf0,label= 'Marcenko-Pastur dist')
-ax.bar(pdf1.index,pdf1.values,width = bWidth,label= 'Empirical dist',color = 'darkorange')
-ax.set_xlabel(r'$\lambda$')
-ax.set_ylabel(r'prob[$\lambda$]')
-ax.legend()
+# bWidth=0.01
+# out=minimize(lambda *x: errPDFs(*x),.5,args=(np.diag(eVal0),q,bWidth),bounds=((1E-5,1-1E-5),))
+# if out['success']:
+#     var=out['x'][0]
+# else:
+#     var=1
+#
+# pdf0=mpPDF(var,q,pts=1000) # Marcenko-Pastur dist
+# pdf1=fitKDE(np.diag(eVal0),bWidth=.01) # empirical pdf
+# ax = plt.figure().add_subplot(111)
+# ax.plot(pdf0,label= 'Marcenko-Pastur dist')
+# ax.bar(pdf1.index,pdf1.values,width = bWidth,label= 'Empirical dist',color = 'darkorange')
+# ax.set_xlabel(r'$\lambda$')
+# ax.set_ylabel(r'prob[$\lambda$]')
+# ax.legend()
 
 
 # ## 2.5 Denoising
@@ -182,21 +169,20 @@ def denoisedCorr(eVal,eVec,nFacts):
     corr1=cov2corr(corr1)
     return corr1
 #--------------------------------------------------- 
-corr1=denoisedCorr(eVal0,eVec0,nFacts0) 
-eVal1,eVec1=getPCA(corr1)
+
 
 
 # In[9]:
 
 
-# A comparison of eigenvalues before and after applying the residual eigenvalue method.
-ax = plt.figure().add_subplot(111)
-ax.plot(np.diagonal(eVal0),label = 'Original eigen-function')
-ax.plot(np.diagonal(eVal1),label = 'Denoised eigen-function (Constant Residual)',linestyle = '--')
-ax.legend()
-ax.set_yscale('log')
-ax.set_xlabel('Eigenvalue number')
-ax.set_ylabel('Eigenvalue (log-scale)')
+# # A comparison of eigenvalues before and after applying the residual eigenvalue method.
+# ax = plt.figure().add_subplot(111)
+# ax.plot(np.diagonal(eVal0),label = 'Original eigen-function')
+# ax.plot(np.diagonal(eVal1),label = 'Denoised eigen-function (Constant Residual)',linestyle = '--')
+# ax.legend()
+# ax.set_yscale('log')
+# ax.set_xlabel('Eigenvalue number')
+# ax.set_ylabel('Eigenvalue (log-scale)')
 
 
 # ### 2.5.2 Targeted Shrinkage
@@ -206,7 +192,7 @@ ax.set_ylabel('Eigenvalue (log-scale)')
 
 
 #SNIPPET 2.6 DENOISING BY TARGETED SHRINKAGE
-def denoisedCorr2(eVal,eVec,nFacts,alpha=0):
+def denoisedCorr2(eVal,eVec,nFacts,alpha=0.0):
 # Remove noise from corr through targeted shrinkage
     eValL,eVecL=eVal[:nFacts,:nFacts],eVec[:,:nFacts] 
     eValR,eVecR=eVal[nFacts:,nFacts:],eVec[:,nFacts:] 
@@ -215,21 +201,20 @@ def denoisedCorr2(eVal,eVec,nFacts,alpha=0):
     corr2=corr0+alpha*corr1+(1-alpha)*np.diag(np.diag(corr1)) 
     return corr2
 #--------------------------------------------------- 
-corr1=denoisedCorr2(eVal0,eVec0,nFacts0,alpha=.5) 
-eVal1,eVec1=getPCA(corr1)
+
 
 
 # In[11]:
 
 
-# A comparison of eigenvalues before and after applying the residual eigenvalue method.
-ax = plt.figure().add_subplot(111)
-ax.plot(np.diagonal(eVal0),label = 'Original eigen-function')
-ax.plot(np.diagonal(eVal1),label = 'Denoised eigen-function (targeted shrinkage)',linestyle = '--')
-ax.legend()
-ax.set_yscale('log')
-ax.set_xlabel('Eigenvalue number')
-ax.set_ylabel('Eigenvalue (log-scale)')
+# # A comparison of eigenvalues before and after applying the residual eigenvalue method.
+# ax = plt.figure().add_subplot(111)
+# ax.plot(np.diagonal(eVal0),label = 'Original eigen-function')
+# ax.plot(np.diagonal(eVal1),label = 'Denoised eigen-function (targeted shrinkage)',linestyle = '--')
+# ax.legend()
+# ax.set_yscale('log')
+# ax.set_xlabel('Eigenvalue number')
+# ax.set_ylabel('Eigenvalue (log-scale)')
 
 
 # # Experimental Results
@@ -315,44 +300,70 @@ def optPort(cov,mu=None): # optimal portfolio for minimum variance
     w=np.dot(inv,mu) 
     w/=np.dot(ones.T,w) 
     return w
-#--------------------------------------------------- 
-nObs,nTrials,bWidth,shrink,minVarPortf=1000,100,.01,False,True 
-w1= w1_s=pd.DataFrame(columns=range(cov0.shape[0]),
-index=range(nTrials),dtype=float) 
-w1_d=w1.copy(deep=True)
-w1_s_d = w1_s.copy(deep = True)
-np.random.seed(0)
-for i in tqdm(range(nTrials)):
-    mu1,cov1=simCovMu(mu0,cov0,nObs,shrink=True)
-    if minVarPortf:
-        mu1=None 
-    cov1_d=deNoiseCov(cov1,nObs*1./cov1.shape[1],bWidth) 
-    w1_s.loc[i]=optPort(cov1,mu1).flatten() 
-    w1_s_d.loc[i]=optPort(cov1_d,mu1).flatten()
-    
-    
-for i in tqdm(range(nTrials)):
-    mu1,cov1=simCovMu(mu0,cov0,nObs,shrink=False)
-    if minVarPortf:
-        mu1=None 
-    cov1_d=deNoiseCov(cov1,nObs*1./cov1.shape[1],bWidth) 
-    w1.loc[i]=optPort(cov1,mu1).flatten() 
-    w1_d.loc[i]=optPort(cov1_d,mu1).flatten()
+# #---------------------------------------------------
+# nObs,nTrials,bWidth,shrink,minVarPortf=1000,100,.01,False,True
+# w1= w1_s=pd.DataFrame(columns=range(cov0.shape[0]),
+# index=range(nTrials),dtype=float)
+# w1_d=w1.copy(deep=True)
+# w1_s_d = w1_s.copy(deep = True)
+# np.random.seed(0)
+# for i in tqdm(range(nTrials)):
+#     mu1,cov1=simCovMu(mu0,cov0,nObs,shrink=True)
+#     if minVarPortf:
+#         mu1=None
+#     cov1_d=deNoiseCov(cov1,nObs*1./cov1.shape[1],bWidth)
+#     w1_s.loc[i]=optPort(cov1,mu1).flatten()
+#     w1_s_d.loc[i]=optPort(cov1_d,mu1).flatten()
+#
+#
+# for i in tqdm(range(nTrials)):
+#     mu1,cov1=simCovMu(mu0,cov0,nObs,shrink=False)
+#     if minVarPortf:
+#         mu1=None
+#     cov1_d=deNoiseCov(cov1,nObs*1./cov1.shape[1],bWidth)
+#     w1.loc[i]=optPort(cov1,mu1).flatten()
+#     w1_d.loc[i]=optPort(cov1_d,mu1).flatten()
 
 
 # In[18]:
 
 
-#SNIPPET 2.11 ROOT-MEAN-SQUARE ERRORS
-w0=optPort(cov0,None if minVarPortf else mu0) 
-w0=np.repeat(w0.T,w1.shape[0],axis=0) 
-rmsd=np.mean((w1-w0).values.flatten()**2)**.5 # RMSE  not shrunk not denoised
-rmsd_d=np.mean((w1_d-w0).values.flatten()**2)**.5 # RMSE not shrunk denoised
-rmsd_s=np.mean((w1_s-w0).values.flatten()**2)**.5 # RMSE shrunk not denoised
-rmsd_s_d=np.mean((w1_s_d-w0).values.flatten()**2)**.5 # RMSE shrunk denoised
+# #SNIPPET 2.11 ROOT-MEAN-SQUARE ERRORS
+# w0=optPort(cov0,None if minVarPortf else mu0)
+# w0=np.repeat(w0.T,w1.shape[0],axis=0)
+# rmsd=np.mean((w1-w0).values.flatten()**2)**.5 # RMSE  not shrunk not denoised
+# rmsd_d=np.mean((w1_d-w0).values.flatten()**2)**.5 # RMSE not shrunk denoised
+# rmsd_s=np.mean((w1_s-w0).values.flatten()**2)**.5 # RMSE shrunk not denoised
+# rmsd_s_d=np.mean((w1_s_d-w0).values.flatten()**2)**.5 # RMSE shrunk denoised
+#
+# res_tab = pd.DataFrame(columns = ['Note denoised','Denoised'],index = ['Not shrunk','Shrunk'],data = np.array([[rmsd,rmsd_d],[rmsd_s,rmsd_s_d]]))
+#
 
-res_tab = pd.DataFrame(columns = ['Note denoised','Denoised'],index = ['Not shrunk','Shrunk'],data = np.array([[rmsd,rmsd_d],[rmsd_s,rmsd_s_d]]))
-res_tab
+# #--------------------------------------------------------
+
+alpha, nCols, nFact, q = .995, 1000, 100, 10
+def init_para():
+
+    cov=np.cov(np.random.normal(size=(nCols*q,nCols)),rowvar=False)
+    cov=alpha*cov+(1-alpha)*getRndCov(nCols,nFact) # noise+signal
+    corr0=cov2corr(cov)
+    eVal0,eVec0=getPCA(corr0)
+    return corr0,eVal0,eVec0
+
+def denoise_init(method = 1):
+    corr0, eVal0, eVec0 = init_para()
+    eMax0,var0=findMaxEval(np.diag(eVal0),q,bWidth=.01)
+    nFacts0=eVal0.shape[0]-np.diag(eVal0)[::-1].searchsorted(eMax0)
+
+    if method==1:
+        corr1=denoisedCorr(eVal0,eVec0,nFacts0)
+        eVal1,eVec1=getPCA(corr1)
+    else:
+        corr1=denoisedCorr2(eVal0,eVec0,nFacts0,alpha=.5)
+        eVal1,eVec1=getPCA(corr1)
+    return corr0,eVal1,eVec1
+
+
 
 
 
